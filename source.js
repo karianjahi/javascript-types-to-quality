68475396ad6610e0471111db69f9b19146ba41e2
karianjahi <kitgaedesigaidi8@gmail.com>
2025-10-17 10:37:05 +0200

    Create a source file and start implementing functions

---
+++ source.js
@@ -0,0 +1,12 @@
+/*
+This is a collection of functions that demonstrate clean code and code quality through naming of functions in the right way ensuring linting and formatting for consistency
+*/
+
+// A function that returns an array consisting of the largest number from each sub-array.
+largestOfAll = (arrayOfArrays) => {
+
+    return arrayOfArrays;
+}
+
+nestedArray = [[2, 5, 10], [3, 9, 8], [6, 1, 2], [3, 5, 21], [7, 7.5, 1.5], [9, 12, 20.9]]
+console.log(largestOfAll())
