24687b65b0912791f8ddaefc554af7b218c33a06
karianjahi <kitgaedesigaidi8@gmail.com>
2025-10-17 10:43:28 +0200

    Write a function that scans an array of arrays for the largetst number

--- source.js
+++ source.js
@@ -4,9 +4,14 @@ This is a collection of functions that demonstrate clean code and code quality t
 
 // A function that returns an array consisting of the largest number from each sub-array.
 largestOfAll = (arrayOfArrays) => {
-
-    return arrayOfArrays;
+    let largestOfAll = arrayOfArrays[0][0]; // initialize maxValue with the first item of first array
+    for (array of arrayOfArrays) {
+        for(item of array){
+            if(item > largestOfAll) largestOfAll = item;
+        }
+    }
+    return largestOfAll;
 }
 
 nestedArray = [[2, 5, 10], [3, 9, 8], [6, 1, 2], [3, 5, 21], [7, 7.5, 1.5], [9, 12, 20.9]]
-console.log(largestOfAll())
+console.log(largestOfAll(nestedArray));
